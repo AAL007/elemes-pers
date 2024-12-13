91f5a0c7ea2df5fbb5bb4ac9a24c0f74328110b1
Alfonsus Adrian <alfonsusal@gmail.com>
2024-12-13 16:34:20 +0700

    fixing issue handleSessionLog

--- src/app/(AddMorePageWithinThisFolder)/course/[...parameters]/page.tsx
+++ src/app/(AddMorePageWithinThisFolder)/course/[...parameters]/page.tsx
@@ -248,7 +248,6 @@ const courseDetailList = ({params} : {params: {parameters: string}}) => {
             }
         }))
         setSessionsList(sessionsList);
-        handleSessionLog(sessionsList[0].sessionId, true, false);
         setTimeout(() => {
             setIsLoading(false);
         }, 11000)
@@ -283,10 +282,8 @@ const courseDetailList = ({params} : {params: {parameters: string}}) => {
 
     useEffect(() => {
         if(sessionId === "") return;
-        if(selected === "content"){
-            handleSessionLog(sessionId, true, false);
-        }else if(selected === "assignment"){
-            handleSessionLog(sessionId, false, true);
+        if(selected === "assignment"){
+            handleSessionLog(sessionId, true, true);
         }
     }, [selected])
 
